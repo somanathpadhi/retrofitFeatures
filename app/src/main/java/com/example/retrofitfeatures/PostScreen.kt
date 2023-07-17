67e77c37c7e05739858c0203694582d0d141e723
Somanath <somanath@Somanaths-MacBook-Pro.local>
2023-07-17 20:53:56 +0530

    compose  progress bar added

--- app/src/main/java/com/example/retrofitfeatures/PostScreen.kt
+++ app/src/main/java/com/example/retrofitfeatures/PostScreen.kt
@@ -1,16 +1,21 @@
 package com.example.retrofitfeatures
 
 import android.util.Log
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,6 +26,14 @@ fun PostScreen(viewModel: PostViewModel){
     when(val result = resultState){
         ResultState.Loading -> {
             Log.d("myLog","Now Loading...")
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                CircularProgressIndicator(
+                    modifier = Modifier.size(32.dp)
+                )
+            }
         }
         ResultState.empty -> {}
         is ResultState.Failure -> {
@@ -52,3 +65,17 @@ fun ItemPreview(){
     val post = Post(id = 1, body = "hey", title = "Hello", userId = 2)
     Item(post = post)
 }
+
+@Preview
+@Composable
+fun CircularProgressIndicatorPreview(){
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier.size(32.dp)
+        )
+    }
+}
+
